5b99ac964eddc18e1b0cf4e7e5b62ddc264a151d
Alejandro E. Losada <alosada@gmail.com>
2014-11-19 19:28:43 -0430

    can send emails

--- js/app.js
+++ js/app.js
@@ -15,25 +15,54 @@ Controller.prototype = {
   },
 
   bindListeners: function(){
-    $('#contact-form').submit(this.sendMail);
+    $('#contact-form').submit(this.sendMailWrapper);
     $('#contact-link').on('click', view.contactIn);
     $('#close').on('click', view.contactOut);
   },
 
-  sendMail: function(e){
+  sendMailWrapper: function(e){
     e.preventDefault();
-    console.log("SendMail function activated")
     var formData = view.getFormData();
-    debugger
+    if (model.prepMail(formData)){
+      view.contactOut()
+      view.dispOkay("Message sent successfully")
+    }
+    else{
+      view.dispError("Oops, there was a problem. Please try again.")
+    }
   }
 }
 
 function Model(){
   this.lasllaves = "w0G3bYcspMgrO8ewpZGelA";
+  this.address = "https://mandrillapp.com/api/1.0/messages/send.json"
   this.i=0;
 }
 
 Model.prototype = {
+  prepMail: function(mail){
+    var data = {
+      key: this.lasllaves,
+      message: mail
+    }
+    return this.sendMail(data)
+  },
+
+  sendMail: function(data){
+    var ajaxReq = $.ajax({
+    url: this.address,
+    type: "POST",
+    data: JSON.stringify(data),
+    }).done(function(r) {
+      if(r[0].status=="sent"){
+        return true
+      }
+      else{
+        return false
+      }
+    })
+  },
+
 }
 
 function View(){}
@@ -60,6 +89,11 @@ View.prototype = {
     };
 
     return formData
+  },
+  dispError: function(text){
+    alert(text)
+  },
+  dispOkay: function(text){
+    alert(text)
   }
-
 }
