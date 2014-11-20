ce10e0f12b52738d231f8c8acb2563c095831228
Alejandro E. Losada <alosada@gmail.com>
2014-11-19 21:57:01 -0430

    workign on animation

--- js/app.js
+++ js/app.js
@@ -16,8 +16,9 @@ Controller.prototype = {
 
   bindListeners: function(){
     $('#contact-form').submit(this.sendMailWrapper);
-    $('#contact-link').on('click', view.contactIn);
-    $('#close').on('click', view.contactOut);
+    $('#contact-link').on('click', view.containerIn);
+    $('#portfolio-link').on('click', view.containerIn);
+    $('.close').on('click', view.close);
   },
 
   sendMailWrapper: function(e){
@@ -68,12 +69,21 @@ Model.prototype = {
 function View(){}
 
 View.prototype = {
-  contactIn: function(){
-    $("#contact-container").fadeIn()
+  containerIn: function(e){
+    e.preventDefault()
+    var prefix = e.target.id.split('-')[0]
+    $("#"+prefix+"-container").fadeIn()
   },
 
   contactOut: function(){
     $("#contact-container").fadeOut()
+  },
+
+  close: function(e){
+    e.preventDefault()
+    var parent = $('#'+e.target.parentElement.id)
+    parent.fadeOut()
+    setTimeout(function(){parent.removeClass('error okay')},1000)
   },
 
   getFormData: function(){
@@ -90,10 +100,109 @@ View.prototype = {
 
     return formData
   },
+
   dispError: function(text){
-    alert(text)
+    $('#alert-box').addClass("error").fadeIn()
+    $('#alert-box span').text(text)
+
   },
+
   dispOkay: function(text){
-    alert(text)
-  }
+    $('#alert-box').addClass("error").fadeIn()
+    $('#alert-box span').text(text)
+  },
+
+  play: function() {
+    this.camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    this.camera.position.z = 5000;
+    this.scene = new THREE.Scene();
+
+    // table
+    for ( var i = 0; i < text.length; i += 3 ) {
+      var letter = document.createElement( 'div' );
+      letter.className = 'letter';
+      letter.textContent = text[i];
+      var object = new THREE.CSS3DObject( letter );
+      object.position.x = ( text[ i + 1 ] * 140 ) - 2200;
+      object.position.y = - ( text[ i + 2 ] * 180 ) + 750;
+      scene.add( object );
+
+      objects.push( object );
+
+      var object = new THREE.Object3D();
+      object.position.x = ( text[ i + 1 ] * 140 ) - 2200;
+      object.position.y = - ( text[ i + 2 ] * 180 ) + 750;
+
+      targets.table.push( object );
+    }
+    // Sphere
+    var vector = new THREE.Vector3();
+    for ( var i = 0, l = objects.length; i < l; i ++ ) {
+      var phi = Math.acos( -1 + ( 2 * i ) / l );
+      var theta = Math.sqrt( l * Math.PI ) * phi;
+      var object = new THREE.Object3D();
+      object.position.x = 800 * Math.cos( theta ) * Math.sin( phi );
+      object.position.y = 800 * Math.sin( theta ) * Math.sin( phi );
+      object.position.z = 800 * Math.cos( phi );
+      vector.copy( object.position ).multiplyScalar( 2 );
+      object.lookAt( vector );
+      targets.sphere.push( object );
+    }
+    // helix
+    var vector = new THREE.Vector3();
+    for ( var i = 0, l = objects.length; i < l; i ++ ) {
+      var phi = i * 0.175 + Math.PI;
+      var object = new THREE.Object3D();
+      object.position.x = 900 * Math.sin( phi );
+      object.position.y = - ( i * 16 ) + 900;
+      object.position.z = 900 * Math.cos( phi );
+      vector.x = object.position.x * 2;
+      vector.y = object.position.y;
+      vector.z = object.position.z * 2;
+      object.lookAt( vector );
+      targets.helix.push( object );
+    }
+
+ /  / doubleHelix
+    var vector = new THREE.Vector3();
+    for ( var i = 0, l = objects.length; i < l; i ++ ) {
+      if (i%2===1){
+        var phi = i * 0.175 + 2*Math.PI;
+      }
+      else {
+        var phi = i * 0.175 + Math.PI;
+      }
+      var object = new THREE.Object3D();
+      object.position.x = 450 * Math.sin( phi );
+      object.position.y = - ( i * 64 ) + 1800;
+      object.position.z = 450 * Math.cos( phi );
+      vector.x = object.position.x * 2;
+      vector.y = object.position.y;
+      vector.z = object.position.z * 2;
+      object.lookAt( vector );
+      targets.doubleHelix.push( object );
+    }
+
+    // grid
+    for ( var i = 0; i < objects.length; i ++ ) {
+      var object = new THREE.Object3D();
+      object.position.x = ( ( i % 5 ) * 400 ) - 800;
+      object.position.y = ( - ( Math.floor( i / 5 ) % 5 ) * 400 ) + 800;
+      object.position.z = ( Math.floor( i / 25 ) ) * 1000 - 2000;
+      targets.grid.push( object );
+    }
+
+    renderer = new THREE.CSS3DRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.domElement.style.position = 'absolute';
+    document.getElementById( 'container' ).appendChild( renderer.domElement );
+
+    controls = new THREE.TrackballControls( camera, renderer.domElement );
+    controls.rotateSpeed = 0.5;
+    controls.minDistance = 500;
+    controls.maxDistance = 6000;
+    controls.addEventListener( 'change', render );
+
+    transform( targets.sphere, 5000 );
+    }
 }
